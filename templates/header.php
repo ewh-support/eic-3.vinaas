560aa62867bab30c3a2efdb1327bf14be6e1d126
vinaas <vinaas@email.com>
2020-01-10 15:56:30 +0700

    Fri 01/10/2020 15:56:26.57

---
+++ templates/header.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="src/css/main.css">
+<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
